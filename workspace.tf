254f3fff21e9a24bd80a89fae6a05f59c1634e84
Jschenk64 <josephschenk.js@gmail.com>
2024-12-20 17:49:43 +0100

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,16 +1,10 @@
 locals {
-  instance_type = tomap({
-    dev                  = "t2.micro"
-    prod                 = "t2.large"
-    test                 = "t2.small"
-    "my-instances-workflow" = "t3.medium"
-  })
+  instance_type = {
+    dev  = "t2.micro"
+    prod = "t2.large"
+    test = "t2.small"
+  }
 }
-
-resource "aws_instance" "ec2type" {
-  instance_type = lookup(local.instance_type, terraform.workspace, "t2.micro")
-}
-
 
 resource "aws_instance" "ec2type" {
     ami = "ami-0a628e1e89aaedf80"
