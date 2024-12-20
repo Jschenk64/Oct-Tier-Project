ad686ad3b9c68842c09fa541d16034c2a51235f8
Jschenk64 <josephschenk.js@gmail.com>
2024-12-20 17:30:38 +0100

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,18 +1,11 @@
 locals {
-  mapped_workspace = {
-    "my-instances-workflow" = "prod"
-  }
   instance_type = {
-    dev  = "t2.micro"
-    prod = "t2.large"
-    test = "t2.small"
+    dev                  = "t2.micro"
+    prod                 = "t2.large"
+    test                 = "t2.small"
+    my-instances-workflow = "t3.medium"
   }
 }
-
-resource "aws_instance" "ec2type" {
-  instance_type = local.instance_type[local.mapped_workspace[terraform.workspace]]
-}
-
 
 resource "aws_instance" "ec2type" {
     ami = "ami-0a628e1e89aaedf80"
